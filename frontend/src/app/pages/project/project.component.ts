a292e8ebe47cefb94eb57708663580244025d305
DIOT Jeremy <diot.jeremy@outlook.com>
2022-10-27 22:20:44 +0200

    style(frontend): apply standard autoformating

--- frontend/src/app/pages/project/project.component.ts
+++ frontend/src/app/pages/project/project.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent implements OnInit {
-  constructor () { }
-
-  ngOnInit (): void {
-  }
-}
+export class ProjectComponent { }
