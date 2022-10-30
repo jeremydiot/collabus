0d2eeb74fc77ef9720b46cbcd803ecae6b0d88e5
Jeremy DIOT <diot.jeremy@gmail.com>
2022-10-30 14:23:30 +0100

    chore(backend): make required email in user model

--- backend/apps/main/models.py
+++ backend/apps/main/models.py
@@ -1,3 +1,16 @@
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(
+        _("email address"),
+        unique=True,
+        help_text=_(
+            "Required. 254 characters or fewer. Email format only."
+        ),
+        error_messages={
+            "unique": _("A user with this email already exists."),
+        },
+    )
