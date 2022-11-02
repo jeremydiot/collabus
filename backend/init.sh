dc81d7e5563826472ca2e1fe7e70dbbf24727a83
Jeremy DIOT <diot.jeremy@gmail.com>
2022-11-02 20:23:42 +0100

    ci(backend): update utils bash scripts

--- backend/init.sh
+++ backend/init.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-ENV_PATH="env/"
+# exit when any command fails
+set -e 
 
+# install and configure python virtual environment
+ENV_PATH="env/"
 [ ! -d $ENV_PATH ] && python3.10 -m venv env && echo "set -a && source .env && set +a">> env/bin/activate
 
+# install python dependencies in virtual environment
 source env/bin/activate
-
 python3 -m pip install -r requirements/common.txt
 [ "$DJANGO_EXECUTION_ENVIRONMENT" = "development" ] && python3 -m pip install -r requirements/development.txt
---
+++ backend/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e 
+
+# start and wait postgres database is running
+docker compose up -d
+until docker exec collabus_dev_postgres psql -U postgres > /dev/null 2>&1; do
+  echo "waiting for postgres container..."
+  sleep 1
+done
+
+# active python virtual environment configure and run server
+source env/bin/activate
+python3 manage.py migrate
+python3 manage.py createsuperuser --noinput
+python3 manage.py runserver
+
+docker compose stop
+# docker compose down # for stop and remove containers
