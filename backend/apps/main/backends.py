d57f6ca214f2083285eb3826594a12bedd8ff7ec
Jeremy DIOT <diot.jeremy@outlook.com>
2022-11-08 21:55:41 +0100

    style(backend): convert variable snakeCase to camel_case

--- backend/apps/main/backends.py
+++ backend/apps/main/backends.py
@@ -5,18 +5,18 @@ from django.db.models import Q
 
 class AuthBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        UserModel = get_user_model()
-        if username is None:
-            username = kwargs.get(UserModel.USERNAME_FIELD) or kwargs.get(UserModel.EMAIL_FIELD)
-        if username is None or password is None:
+        user_model = get_user_model()
+        if not username:
+            username = kwargs.get(user_model.USERNAME_FIELD) or kwargs.get(user_model.EMAIL_FIELD)
+        if not username or not password:
             return
         try:
             # user = UserModel._default_manager.get_by_natural_key(username)
-            user = UserModel.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
-        except UserModel.DoesNotExist:
+            user = user_model.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+        except user_model.DoesNotExist:
             # Run the default password hasher once to reduce the timing
             # difference between an existing and a nonexistent user (#20760).
-            UserModel().set_password(password)
+            user_model().set_password(password)
         else:
             if user.check_password(password) and self.user_can_authenticate(user):
                 return user
