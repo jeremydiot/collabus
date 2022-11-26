02bef9ab887c477b41cf1a5d25e0761545c6f1da
Jeremy DIOT <diot.jeremy@outlook.com>
2022-11-26 11:16:30 +0100

    fix(backend): better modulable api start scriot

--- backend/start.sh
+++ backend/start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# active python virtual environment
+source env/bin/activate
+
 # start and wait postgres database is running
 docker compose up -d
-until docker exec collabus_dev_postgres psql -U postgres > /dev/null 2>&1; do
+until docker exec collabus_dev_postgres psql $DJANGO_DATABASE_NAME $DJANGO_DATABASE_USER > /dev/null 2>&1; do
   echo "waiting for postgres container..."
   sleep 1
 done
 
-# active python virtual environment configure and run server
-source env/bin/activate
+# configure and run server
 python3 manage.py migrate
 python3 manage.py createsuperuser --noinput > /dev/null 2>&1
 python3 manage.py runserver
