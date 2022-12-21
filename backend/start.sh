9cb6ba893a7935db21a453e5f254c5e71bfc1e7d
Jeremy DIOT <diot.jeremy@outlook.com>
2022-12-21 21:37:02 +0100

    ci(backend): better scripts name

--- backend/start.sh
+++ backend/start.sh
@@ -4,12 +4,12 @@
 source env/bin/activate
 
 # start docker
-./scripts/dockerUp.sh
+./scripts/dockerStart.sh
 
 # configure and run server
 python3 manage.py migrate
 python3 manage.py createsuperuser --noinput > /dev/null 2>&1
 python3 manage.py runserver
 
-docker compose stop
-# docker compose down # for stop and remove containers
+# stop docker
+./scripts/dockerStop.sh
