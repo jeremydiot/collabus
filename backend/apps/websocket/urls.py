ce56a320cae86cf2fd8cead2f9c9af93fd1b3c65
Jeremy DIOT <diot.jeremy@outlook.com>
2022-12-30 09:44:12 +0100

    chore(backend): disable websocket chat test url for propduction context

--- backend/apps/websocket/urls.py
+++ backend/apps/websocket/urls.py
@@ -1,7 +1,10 @@
 
 from django.urls import path
+from django.conf import settings
 from apps.websocket import views
 
 urlpatterns = [
-    path('view/chat/<str:kind>/<str:pk>/', views.ChatView.as_view(), name='websocket_chat_view'),
+    *([
+        path('view/chat/<str:kind>/<str:pk>/', views.ChatView.as_view(), name='websocket_chat_view'),
+    ]if settings.EXECUTION_ENVIRONMENT in ['development', 'staging'] else []),
 ]
