a699f3e6564bb5cf110bafb015d6ab19084249cb
Jeremy DIOT <diot.jeremy@outlook.com>
2023-01-24 21:51:17 +0100

    feat(frontend): connexion form style

--- frontend/src/app/pages/login-page/login-page.component.ts
+++ frontend/src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-login-page',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  formGroup = new FormGroup({
+    email: new FormControl('', { nonNullable: true, validators: [Validators.email, Validators.required] }),
+    password: new FormControl('', { nonNullable: true, validators: [Validators.required] })
+  })
+
   constructor () { }
 
   ngOnInit (): void {
+  }
+
+  onSubmit (): void {
+    if (this.formGroup.valid) {
+      console.log('ok')
+    }
   }
 }
