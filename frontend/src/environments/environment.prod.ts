d8a5846b9b10f89625165c67286e160d149e1474
Jeremy DIOT <diot.jeremy@outlook.com>
2023-01-31 19:07:39 +0100

    fix(frontend): secure websocket connexion

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   production: true,
   backendUrl: 'https://backend.collabus.fr/api',
   backendHost: 'https://backend.collabus.fr',
-  backendWebsocketChat: 'ws://backend.collabus.fr/websocket/consumer/chat',
+  backendWebsocketChat: 'wss://backend.collabus.fr/websocket/consumer/chat',
   avatarUrl: 'https://secure.gravatar.com/avatar'
 }
