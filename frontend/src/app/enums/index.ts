4bf3b3f4685d0a388b9968fc1df6707e7ca56734
Jeremy DIOT <diot.jeremy@outlook.com>
2023-03-06 18:55:51 +0100

    chore(frontend): add all project kinds

--- frontend/src/app/enums/index.ts
+++ frontend/src/app/enums/index.ts
@@ -5,9 +5,19 @@ export enum EntityKind {
 }
 
 export enum ProjectKind {
-  'création de site' = 1,
-  'desing d\'interface' = 2,
-  'charte graphique' = 3,
-  'stratégie de communication' = 4,
-  'stratégie digitale' = 5,
+  'Identité visuelle' = 1,
+  'Charte graphique' = 2,
+  'Création de logo' = 3,
+  'Parcours utilisateur' = 4,
+  'Maquettage graphique' = 5,
+  'Développement de site' = 6,
+  'Développement d\'application' = 7,
+  'Développement de logiciel' = 8,
+  'Spécifications technique et fonctionnelle' = 9,
+  'Gestion et organisation de projet' = 10,
+  'Stratégie de communication' = 11,
+  'Stratégie digitale' = 12,
+  'Analyse de marché' = 13,
+  'Campagne publicitaire' = 14,
+  'Stratégie de prix et de distribution' = 15,
 }
